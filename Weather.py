d25cc0e80cfcc9783ecf39bff22fd38ef28e0a2b
Islom-Inomjonov <90414839+Islom-Inomjonov@users.noreply.github.com>
2021-09-14 23:05:22 +0300

    Add files via upload
    My first repository

---
+++ Weather.py
@@ -0,0 +1,13 @@
+is_hot = True
+is_cold = False
+
+if is_hot:
+    print("It is a hot day")
+    print("Drink plenty of water")
+
+elif is_cold:
+    print("It is a cold day")
+    print("Wear warm clothes")
+
+else:
+    print("Enjoy your day!")
